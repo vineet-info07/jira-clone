959dd4175b7858f50d63798dc621e57fd6ed3265
Vineet Tijare <vineettijare@Vineets-MacBook-Air.local>
2025-09-18 15:23:54 +0530

    initial steup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import { Button } from "@/components/ui/button";
-import Test from "@/features/test";
 
 export default function Home() {
   return (
-    <div>
-      <Button>Click me</Button>
-      <p className="text-red font-semibold">Krunal</p>
-      <Test />
+    <div className="">
+      <Button>Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="destructive">destructive</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="muted">Muted</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="teritary">Teritary</Button>
     </div>
   );
 }

1ab114960c1c9ea1b302e1f19d57486467a9fd49
Vineet Tijare <vineettijare@192.168.1.9>
2025-10-10 13:36:07 +0530

    adding seperator and sign-in-cart

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,8 +1,23 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 interface AuthLayoutProps {
   children: React.ReactNode;
 }
 
 const AuthLayout = ({ children }: AuthLayoutProps) => {
-  return <div>{children}</div>;
+  return (
+    <main className="bg-neutral-100 min-h-screen">
+      <div className="mx-auto max-w-screen-2xl p-4">
+        <nav className="flex justify-between items -center">
+          <Image src="/logo.svg" height={56} width={152} alt="logo" />
+          <Button variant="secondary">Sign Up</Button>
+        </nav>
+        <div className="flex flex-col items-center justify-center pt-4 md:pt-14">
+          {children}
+        </div>
+      </div>
+    </main>
+  );
 };
 export default AuthLayout;
---
+++ src/features/auth/components/sign-in-card.tsx
@@ -0,0 +1,28 @@
+import { DottedSeparator } from "@/components/dotted-seperator";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+
+export const SignInCard = () => {
+  return (
+    <Card className="w-full h-full md:w-[487px] border-none shadow-none">
+      <CardHeader className="flex items-center jusity-center text-center p-7">
+        <CardTitle className="text-2xl">Welcome back!</CardTitle>
+      </CardHeader>
+      <div className="px-7">
+        <DottedSeparator />
+      </div>
+      <CardContent className="p-7">
+        <form className="space-y-4">
+          <Input
+            required
+            type="email"
+            value={""}
+            onChange={() => {}}
+            placeholder="Enter email address"
+            disabled={false}
+          />
+        </form>
+      </CardContent>
+    </Card>
+  );
+};
